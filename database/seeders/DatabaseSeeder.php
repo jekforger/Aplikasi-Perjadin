054cb9668ca3bc5f55fea7458891da3d395c715f
jekforger <mudzaqimaulana@gmail.com>
2025-06-13 20:58:20 +0700

    first commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +12,57 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::create([
+    'name' => 'Fahrizal Mudzaqi M',
+    'email' => 'fahrizal.mudzaqi.tif22@polban.ac.id',
+    'password' => bcrypt('pengusul123'),
+    'role' => 'pengusul',
+    ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+    \App\Models\User::create([
+        'name' => 'Faras Rama Mahadika',
+        'email' => 'faras@polban.ac.id',
+        'password' => bcrypt('wadir1123'),
+        'role' => 'wadir_1',
+    ]);
+
+    \App\Models\User::create([
+        'name' => 'Angeline Putri',
+        'email' => 'angeline@polban.ac.id',
+        'password' => bcrypt('wadir123'),
+        'role' => 'wadir',
+    ]);
+
+    \App\Models\User::create([
+        'name' => 'Naufal Syafiq S',
+        'email' => 'naufal.syafiq.tif22@polban.ac.id',
+        'password' => bcrypt('direktur123'),
+        'role' => 'direktur',
+    ]);
+
+    \App\Models\User::create([
+        'name' => 'Muhammad Syaifullah',
+        'email' => 'syaifullah@polban.ac.id',
+        'password' => bcrypt('direktur123'),
+        'role' => 'bku',
+    ]);
+
+    \App\Models\User::create([
+        'name' => 'Adrian Eka',
+        'email' => 'adrian@polban.ac.id',
+        'password' => bcrypt('direktur123'),
+        'role' => 'admin',
+    ]);
+
+    \App\Models\User::create([
+        'name' => 'Ahmad Fauzy',
+        'email' => 'fauzy@polban.ac.id',
+        'password' => bcrypt('direktur123'),
+        'role' => 'pelaksana',
+    ]);
+        $this->call([
+            PegawaiSeeder::class,
+            MahasiswaSeeder::class,
+        ]);
     }
 }
